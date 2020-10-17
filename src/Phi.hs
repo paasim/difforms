0a2714ab810ec958c936b13271fea2b4d6e3200f
Markus Paasiniemi <markus.paasiniemi@cs.helsinki.fi>
2020-10-17 14:25:35 +0300

    Wrapped Phi inside a newtype for improved priting etc

--- src/Phi.hs
+++ src/Phi.hs
@@ -9,16 +9,40 @@ import Data.Type.Nat ( Nat(..) )
 import qualified Data.Type.Nat as N
 import Data.Fin ( Fin(..) )
 import Data.List.NonEmpty ( NonEmpty(..) )
+import qualified Data.List as L ( intercalate )
+import Test.QuickCheck
 import Typeclasses
 import R
 import C
 import V
 
 -- Linear maps (instead of any diffeomorphism)
-type Phi' n m = Mat n m --Phi' { matPhi :: Vec m (Vec n Rational) }
+newtype Phi n m = Phi { phiMat :: Mat n m } deriving (Eq, Ord)
+
+showElemAsFun :: Fin n -> Rational -> String
+showElemAsFun n r = show r <> "*x_" <> show n
+
+showVecAsFun :: N.SNatI n => R n -> String
+showVecAsFun = L.intercalate " + " . V.toList . fmap (uncurry showElemAsFun) . V.zipWith (,) V.universe . x
+
+showStrAsFun :: Fin n -> String -> String
+showStrAsFun n str = str <> " -> x_" <> show n
+
+showMatAsFun :: (N.SNatI n, N.SNatI m) => Mat n m -> String
+showMatAsFun = L.intercalate "\n" . V.toList . fmap (uncurry showStrAsFun)
+             . V.zipWith (,) V.universe . fmap showVecAsFun . mat . transpose
+
+instance (N.SNatI n, N.SNatI m) => Show (Phi n m) where
+  show = showMatAsFun . phiMat
+
+idPhi :: N.SNatI n => Phi n n
+idPhi = Phi diagMat
+
+compPhi :: (N.SNatI n, N.SNatI m, N.SNatI l) => Phi n m -> Phi m l -> Phi n l
+compPhi (Phi matNM) (Phi matML) = Phi $ matMatProduct matNM matML
 
 -- not endomap due to term not containing sums
-pullbackTerm :: N.SNatI m => Phi' n m -> Term m -> Terms n
+pullbackTerm :: N.SNatI m => Phi n m -> Term m -> Terms n
 pullbackTerm _   (Term d [])             = liftToTerms . liftToTerm $ d
 pullbackTerm phi (Term d (Var ind exp : ts)) =
   -- given coefficient d' and index i, constructs a term
@@ -26,15 +50,17 @@ pullbackTerm phi (Term d (Var ind exp : ts)) =
   -- construct the sum of terms given a cofficient vector (of type R n)
       sumTerms = V.ifoldr (\i d' -> (<>) (termWithCoef i d')) mempty . x
   -- picks the correct vector from phi and multiplies with the other terms recursively
-  in sappend (nthPower (exp+1) . sumTerms $ mat (transpose phi) V.! ind)
+  in sappend (nthPower (exp+1) . sumTerms $ mat (transpose . phiMat $ phi) V.! ind)
              (pullbackTerm phi $ Term d ts)
 
+instance (N.SNatI n, N.SNatI m) => Arbitrary (Phi n m) where
+  arbitrary = Phi <$> arbitrary
 -- precomposes f with the manifold map
-pullback :: N.SNatI m => Phi' n m -> Terms m -> Terms n
+pullback :: N.SNatI m => Phi n m -> Terms m -> Terms n
 pullback phi (Terms (t1 :| []))    = pullbackTerm phi t1
 pullback phi (Terms (t1 :| t2:ts)) = pullbackTerm phi t1 <> pullback phi (Terms (t2 :| ts))
 
 
-pushforward :: (N.SNatI n, N.SNatI m) => Phi' n m -> V n -> V m
-pushforward m v = V $ vecMatProduct (vCoeff v) m
+pushforward :: (N.SNatI n, N.SNatI m) => Phi n m -> V n -> V m
+pushforward phi v = V $ vecMatProduct (vCoeff v) (phiMat phi)
 
--- test/TestPhi.hs
+++ test/TestPhi.hs
@@ -9,40 +9,40 @@ import V
 import Phi
 import TestHelpers
 
-type PullbackDef n m = Phi' n m -> Terms m -> R n -> Bool
+type PullbackDef n m = Phi n m -> Terms m -> R n -> Bool
 
 pullbackDef :: (N.SNatI n, N.SNatI m) => PullbackDef n m
 pullbackDef phi tsm rn = evalTerms (pullback phi tsm) rn ==
-                         evalTerms tsm (vecMatProduct rn phi)
+                         evalTerms tsm (vecMatProduct rn . phiMat $ phi)
 
 
-type PushforwardDef n m = Phi' n m -> V n -> Terms m -> R n -> Bool
+type PushforwardDef n m = Phi n m -> V n -> Terms m -> R n -> Bool
 
 pushforwardDef :: (N.SNatI n, N.SNatI m) => PushforwardDef n m
 pushforwardDef phi vn tsm rn =
-  evalTerms (evalV (pushforward phi vn) tsm) (vecMatProduct rn phi) ==
+  evalTerms (evalV (pushforward phi vn) tsm) (vecMatProduct rn . phiMat $ phi) ==
     evalTerms (evalV vn $ pullback phi tsm) rn
 
 -- tests for:
 -- pushforward is a functor
 type PushforwardId n = V n -> Terms n -> Bool
 pushforwardId :: N.SNatI n => PushforwardId n
-pushforwardId v ts = evalV (pushforward diagMat v) ts == evalV v ts
+pushforwardId v ts = evalV (pushforward idPhi v) ts == evalV v ts
 
-type PushforwardComp n m l = Phi' n m -> Phi' m l -> V n -> Terms l -> Bool
+type PushforwardComp n m l = Phi n m -> Phi m l -> V n -> Terms l -> Bool
 pushforwardComp :: (N.SNatI n, N.SNatI m, N.SNatI l) => PushforwardComp n m l
 pushforwardComp phiNM phiML vn tsl =
-  evalV (pushforward (matMatProduct phiNM phiML) vn) tsl ==
+  evalV (pushforward (compPhi phiNM phiML) vn) tsl ==
     evalV (pushforward phiML . pushforward phiNM $ vn) tsl
 
 -- pushforward is linear
-type PushforwardMult n m = Phi' n m -> Rational -> V n -> Terms m -> Bool
+type PushforwardMult n m = Phi n m -> Rational -> V n -> Terms m -> Bool
 pushforwardMult :: (N.SNatI n, N.SNatI m) => PushforwardMult n m
 pushforwardMult phi r v ts = evalV (vsmult r . pushforward phi $ v) ts
                           == evalV (pushforward phi . vsmult r $ v) ts
 
-type PushforwardAdd n m = Phi' n m -> V n -> V n -> Terms m -> Bool
-pushforwardAdd :: (N.SNatI n, N.SNatI m) => Phi' n m -> V n -> V n -> Terms m -> Bool
+type PushforwardAdd n m = Phi n m -> V n -> V n -> Terms m -> Bool
+pushforwardAdd :: (N.SNatI n, N.SNatI m) => Phi n m -> V n -> V n -> Terms m -> Bool
 pushforwardAdd phi v1 v2 ts =
   evalV (pushforward phi . vsadd v1 $ v2) ts ==
     evalV (vsadd (pushforward phi v1) (pushforward phi v2)) ts
