8943ff56603b11ca1fc3e3c3d363388c5ced7556
Markus Paasiniemi <markus.paasiniemi@cs.helsinki.fi>
2020-10-17 10:14:22 +0300

    Fixed the test for pushforwards

--- test/TestPhi.hs
+++ test/TestPhi.hs
@@ -15,11 +15,14 @@ pullbackDef :: (N.SNatI n, N.SNatI m) => PullbackType n m
 pullbackDef phi tsm rn = evalTerms (pullback phi tsm) rn ==
                          evalTerms tsm (vecMatProduct rn phi)
 
-type PushforwardType n m = Phi' n m -> V n -> Terms m -> R m -> Bool
+
+type PushforwardType n m = Phi' n m -> V n -> Terms m -> R n -> Bool
+
 pushforwardDef :: (N.SNatI n, N.SNatI m) => PushforwardType n m
-pushforwardDef phi vn tsm rm =
-  evalTerms (evalV (pushforward phi vn) tsm) rm ==
-    evalTerms (evalV vn $ pullback phi tsm) (matVecProduct phi rm)
+pushforwardDef phi vn tsm rn =
+  evalTerms (evalV (pushforward phi vn) tsm) (vecMatProduct rn phi) ==
+    evalTerms (evalV vn $ pullback phi tsm) rn
+
 
 main :: IO ()
 main = do
